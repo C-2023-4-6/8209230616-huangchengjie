4bc6ba406ec3abf3c0c5b180651af2eb8f91b3ad
huangchengjie666 <149994601+huangchengjie666@users.noreply.github.com>
2023-11-19 20:58:50 +0800

    Add files via upload

--- 实验二8.cpp
+++ 实验二8.cpp
@@ -3,14 +3,17 @@ using namespace std;
 int main()
 {
 	float a;
-	cout << "请输入一个非负数" << endl;
+	cout << "请输入一个数" << endl;
 	cin >> a;
-	float b=a , c = (a + 1) / 2;
+	float b=fabs(a) , c = (fabs(a) + 1) / 2;
 	while (fabs(c - b)>= 1e-5)
 	{
 		b = c;
-		c = (b + a /b) / 2;
+		c = (b + fabs(a) /b) / 2;
 	}
+	if (a >= 0)
 		cout << "此数的平方根= " << c << endl;
+	else
+		cout << "此数的平方根= " << c << "i" << endl;
 	return 0;
 }
